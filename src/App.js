04354902661e4e546bf379834fa4998346e2f74f
julien willette <julien.willette@gmail.com>
2021-04-15 20:10:14 +0200

    add Navbar and QuoteCards

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Navbar from './components/Navbar';
+import QuoteCards from './components/QuoteCard';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
+      <QuoteCards />
     </div>
   );
 }
