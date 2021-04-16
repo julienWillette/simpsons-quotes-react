643dbdf7fa0a55938be87eb01964e03e7649383f
julien willette <julien.willette@gmail.com>
2021-04-16 09:24:14 +0200

    add QuoteList

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import QuoteCard from './components/QuoteCard';
+import QuoteList from './components/QuoteList';
 
 function App() {
   return (
