70e8123ce0b03ebbcdddbb281fe1977a6e2b9013
julien willette <julien.willette@gmail.com>
2021-05-12 15:05:54 +0200

    add input in quoteForm

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import QuoteCard from './components/QuoteCard';
 import QuoteList from './components/QuoteList';
+import QuoteForm from './components/QuoteForm';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <QuoteForm />
       <QuoteList />
     </div>
   );
